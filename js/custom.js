cd6cc427f390355aa351e1b9f987c71339d977be
Belloaise Carrington <Bello@anniecanonssMBP.hsd1.ga.comcast.net>
2024-06-28 18:25:24 -0400

    intial commit

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+//Toggle nav bar below 
+const burger = document.querySelector(".burger i");
+console.log(burger);
+const nav = document.querySelector(".nav");
+console.log(nav);
+
+function showHamburger() {
+  burger.classList.toggle("fa-times");
+  nav.classList.toggle("navactive");
+
+}
+
+
+// form submit below
+
+const form = document.getElementById("form");
+let userArray = [];
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+	event.preventDefault();
+	const data = new FormData(event.target);
+	const dataObject = Object.fromEntries(data.entries());
+
+	//above is boiler plate code
+	userArray.push(dataObject);
+	form.reset();
+}
